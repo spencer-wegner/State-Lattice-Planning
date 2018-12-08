881c2da4fc1e7e4066a7df23bb1d0e588ac8bbca
Mark Ivlev <markivlev@gmail.com>
2018-12-07 17:26:44 -0700

    doin stuff

--- slp.py
+++ slp.py
@@ -1,7 +1,19 @@
 import numpy as np
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 import heapq
 
-state_lattice = {}
+c='c'
+l='l'
+r='r'
+n='n'
+s='s'
+e='e'
+w='w'
+
+state_lattice = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+for i in range(10):
+    print(state_lattice[i])
+
+graph={(0,0,c,n):[(1,0,c,n),(0,0,c,n),(0,0,r,n)]}
 
 #god damnit why is this so confusing, I feel like it shouldn't be, ya know?
