dfebf644a415316e85d13b121e800e921530bc58
spencer-wegner <spwe4745@colorado.edu>
2018-12-08 10:24:45 -0700

    built out the state graph with just the nodes (keys), not the neighbors (values) yet

--- slp.py
+++ slp.py
@@ -2,18 +2,42 @@ import numpy as np
 #import matplotlib.pylab as plt
 import heapq
 
-c='c'
-l='l'
-r='r'
-n='n'
-s='s'
-e='e'
-w='w'
+# agent wheel direction
+c='center' # 0 degrees, straight ahead
+l='left' # -45 degrees, angled left
+r='right' # 45 degrees, angled right
+angle = [c,l,r]
+# agent heading/direction
+# reflected over x-axis because state lattice (list of lists) access is
+# reversed (we will stay consistent with coordinate system defined by
+# state lattice)
+#       S
+#       |
+#   W -   - E
+#       |
+#       N
+n='north'
+s='south'
+e='east'
+w='west'
+heading = [n,s,e,w]
+# in physical space, agent can only move to different (x,y) coordinate or
+# remain at same (x,y) coordinate and change wheel direction - cannot do both
+# at the same time
 
+# build state lattice
 state_lattice = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
 for i in range(10):
     print(state_lattice[i])
 
-graph={(0,0,c,n):[(1,0,c,n),(0,0,c,n),(0,0,r,n)]}
+# build state graph
+state_graph = {}
+# create all nodes
+for x in range(len(state_lattice[0])):
+    for y in range(len(state_lattice)):
+        for h in heading:
+            for a in angle:
+                state_graph[(x,y,h,a)] = []
 
-#god damnit why is this so confusing, I feel like it shouldn't be, ya know?
+print("state_graph = \n", state_graph)
+print("# of nodes = ", len(state_graph))
