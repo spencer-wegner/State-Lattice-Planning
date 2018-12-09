0731104d2333289468c728cfe80541527f7a73ef
spencer-wegner <spwe4745@colorado.edu>
2018-12-09 16:23:22 -0700

    set up structure for assigning edges

--- slp.py
+++ slp.py
@@ -9,9 +9,11 @@ l='left' # -45 degrees, angled left
 r='right' # 45 degrees, angled right
 angle = [c,l,r]
 # agent heading/direction
-# reflected over x-axis because state lattice (list of lists) access is
+# reflected over normal x-axis because state lattice (list of lists) access is
 # reversed (we will stay consistent with coordinate system defined by
 # state lattice)
+# because of the way python accesses list of lists (choose row and then column),
+# the x-axis is now along the S-N axis and the y-axis in now along the W-E axis
 #       S
 #       |
 #   W -   - E
@@ -46,7 +48,7 @@ def build_state_lattice(nrows, ncols):
 
     return state_lattice
 
-# build state graph
+# this function constructs the state graph
 def build_state_graph(state_lattice):
     state_graph = {}
     # create all nodes
@@ -58,16 +60,41 @@ def build_state_graph(state_lattice):
 
     print("# of nodes = ", len(state_graph))
 
-    # create edges in state graph
+    # create edges between nodes
+    for node in state_graph:
+        assign_edges(node, state_lattice)
+
     return state_graph
 
+# this function creates the edges in the state graph
+def assign_edges(node, state_lattice):
+    if (node[0] == 0 and node[1] == 0): # upper left corner
+        pass
+    elif (node[0] == (len(state_lattice) - 1) and node[1] == 0): # bottom left corner
+        pass
+    elif (node[0] == 0 and node[1] == (len(state_lattice[0]) - 1)): # upper right corner
+        pass
+    elif (node[0] == (len(state_lattice) - 1) and node[1] == (len(state_lattice[0]) - 1)): # bottom right corner
+        pass
+    elif node[0] == 0: # top edge
+        pass
+    elif node[0] == (len(state_lattice) - 1): # bottom edge
+        pass
+    elif node[1] == 0: # left edge
+        pass
+    elif node[1] = (len(state_lattice[0]) - 1): # right edge
+        pass
+    else: # everything else in the middle
+        pass
+        
 # main function
 def main():
     # build a state lattice
     nrows = 10
     ncols = 10
     state_lattice = build_state_lattice(nrows, ncols)
-    state_graph = build_state_graph(state_lattice)
+    print(len(state_lattice[0]))
+    #state_graph = build_state_graph(state_lattice)
 
 if __name__ == '__main__':
     main()
