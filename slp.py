577ea31a134677c5e8daaf79f2ca7c3425fd31ca
spencer-wegner <spwe4745@colorado.edu>
2018-12-10 10:24:26 -0700

    added part of a star

--- slp.py
+++ slp.py
@@ -558,6 +558,78 @@ def assign_edges(state_lattice, state_graph):
                 state_graph[node].append((node[0]-1, node[1]-1, s, l))
     return state_graph
 
+# this function constructs a path that the agent follows through the state space
+def path(previous, s):
+    '''
+    'previous' is a dictionary chaining together the predecessor state that led
+    to each state - 's' will be None for the initial state
+    otherwise, start from the last state 's' and recursively trace 'previous'
+    back to the initial state, constructing a list of states visited as we go
+    '''
+    if s is None:
+        return []
+    else:
+        return path(previous, previous[s]) + [s]
+
+# this function calculates the total cost of the path that the agent takes
+def pathcost(path, step_costs):
+    '''
+    add up the step costs along a path, which is assumed to be a list output
+    from the 'path' function above
+    '''
+    cost = 0
+    for s in range(len(path) - 1):
+        cost += step_costs[path[s]][path[s+1]]
+    return cost
+
+# this class provides methods to initialize and edit the priority queue
+# that is used in A* search
+class Frontier_PQ:
+    def __init__(self, start, cost = 0):
+        self.start = start
+        # initialize dictionary - keys are states (x,y,h,a) and values are
+        # minimum distances to those states from the start state
+        self.states = dict({start : 0})
+        self.q = [(0, start)] # initialize priority queue (cost, state)
+    def add(self, state, cost): # add a (cost, state) tuple to the frontier
+        heapq.heappush(self.q, (cost, state))
+    def pop(self): # return the lowest cost (cost, state) tuple, and pop it off of the frontier
+        return heapq.heappop(self.q)
+    # if a lower path cost to a state already on the frontier is found, it should be replaced
+    def replace(self, state, cost):
+        self.states[state] = cost
+
+def astar_search(start, goal, state_graph, state_lattice, heuristic, return_cost = False, return_nexp = False):
+    my_frontier = Frontier_PQ(start) # create a priority queue
+    visited = []
+    prev = {start : None} # initialize prev dictionary (keys are successors, values are predecessors)
+    while (my_frontier.q): # while the priority queue is not empty
+        x = my_frontier.pop() # pop state off of queue (with heapq it will be the lowest cost tuple)
+        if x[1] not in visited: # if we haven't visited the state yet
+            visited.append(x[1])
+            if x[1] == goal: # We found it!
+                if return_nexp: # return number of nodes expanded
+                    solution_path = path(prev, x[1])
+                    nexp = len(visited) # number of nodes expanded is the number of nodes visited
+                    if return_cost:
+                        # *** if we want to use this we will need to adjust definition of state_graph - costs equal 1
+                        path_cost = pathcost(solution_path, state_graph)
+                        return (solution_path, path_cost, nexp)
+                    else:
+                        return (solution_path, nexp)
+                else:
+                    if return_cost:
+                        solution_path = path(prev, x[1])
+                        # *** if we want to use this we will need to adjust definition of state_graph - costs equal 1
+                        path_cost = pathcost(solution_path, state_graph)
+                        return (solution_path, path_cost)
+                    else:
+                        return path(prev, x[1])
+            else: # we haven't found the goal yet...
+                for neighbor in state_graph[x[1]]:
+                    
+
+
 # main function
 def main():
     # build a state lattice
